975e86c464c174411012c2f5be829d51dd3e7b6f
Alexandra Navarro <ale.navarro53@gmail.com>
2023-07-31 00:29:15 -0400

    [Alexandra Navarro] - Inicialización del proyecto con Maven

---
+++ ProyectoDBD/src/test/java/com/example/ProyectoDBD/ProyectoDbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoDBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
